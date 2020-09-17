32eabbcc1ccb7dad3bf6f018729de79c3c8da280
nguyenduy2392 <nguyenduy2392@gmail.com>
2020-09-17 14:10:36 +0700

    cut all icon

--- component/CategoryView.js
+++ component/CategoryView.js
@@ -13,7 +13,9 @@ const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
     categoryView: {
         position: 'relative',
-        flex: 1,
+        // flex: 1,
+        width: Dimensions.get('window').width,
+        height: 300
     },
     categoryPageView: {
         width: width,
@@ -27,128 +29,43 @@ const styles = StyleSheet.create({
     labelText: {
         color: '#aaa',
         fontWeight: 'bold'
+    },
+    recyclerListViewContent: {
+        paddingBottom: 20
     }
 });
 
-const CategoryView = ({
-    category,
-    emojis,
-    numRows,
-    numCols,
-    emojiSize,
-    labelStyle,
-    onClick
-}) => {
-    const [toggleSkinBox, setToggleSkinBox] = useState({
-        showSkinBox: false,
-        emoji: null
-    });
-    // Emoji count per page
-    const perPage = numRows * numCols;
-    const emojiWidth = (width - 20) / numRows;
-    const clickEmoji = emoji => {
-        setToggleSkinBox({
-            showSkinBox: false,
-            emoji: null
-        });
-        onClick(emoji);
-    };
-
-    const longPressEmoji = emoji => {
-        if (emoji.skins) {
-            setToggleSkinBox({
-                showSkinBox: true,
-                emoji
-            });
-        } else {
-            onClick(emoji);
-        }
-    };
-
-    const tabBar = () => {
-        return (
-            <View style={styles.categoryLabel}>
-                <Text style={[styles.labelText, labelStyle]}>{category}</Text>
-            </View>
-        );
-    };
-
-    const layoutProviderNews = new LayoutProvider(
+class CategoryView extends React.PureComponent {
+    layoutProviderNews = new LayoutProvider(
         (index => 0),
         (type, dim) => {
-            dim.width = width / numRows;
+            dim.width = (width -1) / 8;
             dim.height = 40;
         }
     );
-    let dataProvider = new DataProvider((r1, r2) => {
+    dataProvider = new DataProvider((r1, r2) => {
         return r1 !== r2;
     });
-
-    const rowRendererNews = (type, data) => {
+    rowRendererNews = (type, data) => {
         return <EmojiIcon
             emoji={data}
-            clickEmoji={clickEmoji}
-            longPressEmoji={longPressEmoji}
-            emojiWidth={emojiWidth}
-            emojiSize={emojiSize}
+            clickEmoji={this.props.onClick}
         />
     }
+    render() {
+        return (
+            <View style={styles.categoryView}>
+                <RecyclerListView
+                    contentContainerStyle={styles.recyclerListViewContent}
+                    layoutProvider={this.layoutProviderNews}
+                    dataProvider={this.dataProvider.cloneWithRows(this.props.emojis)}
+                    rowRenderer={this.rowRendererNews}
 
-    const renderCategory = () => {
-        if (!emojis.length) {
-            return <View />;
-        }
-        const pages = Math.ceil(emojis.length / perPage);
-        const categoryView = [];
-        for (let i = 0; i < pages; i++) {
-            const currentPageEmojis = _.slice(
-                emojis,
-                i * perPage,
-                (i + 1) * perPage
-            );
-            categoryView.push(
-                <View
-                    style={styles.categoryPageView}
-                    key={`page-${i}`}
-                    tabLabel={`page-${i}`}>
-                    
-                    <RecyclerListView
-                        contentContainerStyle={styles.recyclerListViewContent}
-                        layoutProvider={layoutProviderNews}
-                        dataProvider={dataProvider.cloneWithRows(currentPageEmojis)}
-                        rowRenderer={rowRendererNews}
-
-                    />
-                </View>
-            );
-        }
-        return categoryView;
-    };
-    return (
-        <View tabLabel={category} style={styles.categoryView}>
-            {toggleSkinBox.showSkinBox && (
-                <SkinBox
-                    emoji={toggleSkinBox.emoji}
-                    clickEmoji={clickEmoji}
-                    emojiWidth={emojiWidth}
-                    emojiSize={emojiSize}
                 />
-            )}
-            <Carousel
-                autoplay={false}
-                loop
-                index={0}
-                pageSize={width}
-            >
-                {renderCategory()}
-            </Carousel>
-
-        </View>
-    );
-};
-
-CategoryView.propTypes = {
-    category: PropTypes.string
+            </View>
+        )
+    }
 };
 
 export default CategoryView;
+
